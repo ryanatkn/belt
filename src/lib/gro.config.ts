2427ba37a18872886e920c5bf6a347b96852eefb
Ryan Atkinson <mail@ryanatkn.com>
2022-11-11 00:38:48 -0600

    todo

--- src/lib/gro.config.ts
+++ src/lib/gro.config.ts
@@ -3,8 +3,6 @@ import type {GroConfigCreator, GroConfigPartial} from '@feltcoop/gro';
 import {LogLevel} from '$lib/log';
 import {exports} from '$lib/exports';
 
-// TODO render this data
-
 const config: GroConfigCreator = async ({dev}) => {
 	const partial: GroConfigPartial = {
 		builds: [dev ? null : {name: 'library', platform: 'node', input: exports}],
