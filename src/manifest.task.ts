dcfc4d9dd4863a96554e96f5aabdfd1eed21583e
Ryan Atkinson <mail@ryanatkn.com>
2022-11-14 14:04:49 -0600

    print back out

--- src/manifest.task.ts
+++ src/manifest.task.ts
@@ -1,4 +1,5 @@
 import type {Task} from '@feltcoop/gro';
+import ts from 'typescript';
 
 import {exports} from '$lib/exports';
 
@@ -8,7 +9,14 @@ export const task: Task = {
 		for (const exp of exports) {
 			const importPath = 'src/' + exp;
 			const contents = await fs.readFile(importPath, 'utf8');
-			console.log(`contents.length`, exp, contents.length);
+			console.log(exp, contents.length);
+
+			const node = ts.createSourceFile(importPath, contents, ts.ScriptTarget.Latest);
+			console.log(`node`, node.statements);
+			const printer = ts.createPrinter({newLine: ts.NewLineKind.LineFeed});
+			const printed = printer.printNode(ts.EmitHint.Unspecified, node, node);
+			console.log(`printed`, printed.length);
+			// await fs.writeFile(importPath + '.ts', printed, 'utf8');
 		}
 	},
 };
