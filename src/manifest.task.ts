8908ba08d686013a5964194b15aa5a40c4acf999
Ryan Atkinson <mail@ryanatkn.com>
2022-11-14 18:01:57 -0600

    wip

--- src/manifest.task.ts
+++ src/manifest.task.ts
@@ -7,15 +7,18 @@ export const task: Task = {
 	run: async ({fs}) => {
 		console.log(`exports`, exports);
 		for (const exp of exports) {
+			if ('lib/json.ts' !== exp) continue;
 			const importPath = 'src/' + exp;
 			const contents = await fs.readFile(importPath, 'utf8');
 			console.log(exp, contents.length);
 
 			const node = ts.createSourceFile(importPath, contents, ts.ScriptTarget.Latest);
 			console.log(`node`, node.statements);
-			const printer = ts.createPrinter({newLine: ts.NewLineKind.LineFeed});
-			const printed = printer.printNode(ts.EmitHint.Unspecified, node, node);
-			console.log(`printed`, printed.length);
+			for (const s of node.statements) {
+				const printer = ts.createPrinter({newLine: ts.NewLineKind.LineFeed});
+				const printed = printer.printNode(ts.EmitHint.Unspecified, s, node);
+				console.log(`printed`, printed);
+			}
 			// await fs.writeFile(importPath + '.ts', printed, 'utf8');
 		}
 	},
