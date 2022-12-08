c1d6ae56baed3f3129542538cbc11531912eac8e
Ryan Atkinson <mail@ryanatkn.com>
2022-12-08 15:18:28 -0600

    add nexter array util

--- src/lib/array.ts
+++ src/lib/array.ts
@@ -6,3 +6,18 @@ export const removeUnordered = (array: any[], index: number): void => {
 	array[index] = array[array.length - 1];
 	array.pop();
 };
+
+/**
+ * Returns a function that returns the next item in the `array`
+ * in a linear sequence, looping back to index 0 when it reaches the end.
+ * @param array
+ * @returns A function that returns the next item in the `array`
+ */
+export const nexter = <T>(array: T[]): (() => T) => {
+	let i = -1;
+	return () => {
+		i++;
+		if (i >= array.length) i = 0;
+		return array[i];
+	};
+};
