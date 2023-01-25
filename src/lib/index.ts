6eb486a8fa665e8fd578832303a2a875c6d46826
Ryan Atkinson <mail@ryanatkn.com>
2023-01-25 15:30:53 -0700

    rename

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,5 @@
 // Individual module exports are listed at `src/lib/exports.ts`,
-// e.g. `import Dialog from '@feltcoop/felt/dialog/Dialog.svelte'`
+// e.g. `import Dialog from '@feltjs/felt-ui/dialog/Dialog.svelte'`
 
 // The public API for `@feltjs/util`:
 
