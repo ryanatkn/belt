a473bbdccc05b6030c5cb4813ba727dc8b7189f5
Ryan Atkinson <mail@ryanatkn.com>
2023-09-20 20:07:11 -0600

    wip

--- src/lib/log.ts
+++ src/lib/log.ts
@@ -39,9 +39,7 @@ export const configureLogLevel = (
 };
 
 const DEFAULT_LOG_LEVEL: LogLevel =
-	(typeof process !== 'undefined' && process.env?.PUBLIC_LOG_LEVEL) ??
-	import.meta.env?.PUBLIC_LOG_LEVEL ??
-	'info';
+	process?.env?.PUBLIC_LOG_LEVEL ?? import.meta.env?.PUBLIC_LOG_LEVEL ?? 'info';
 
 /*
 
