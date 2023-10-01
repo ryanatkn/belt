f3da3979f5d1e179780971f613f6dc8fb3174b4e
Ryan Atkinson <mail@ryanatkn.com>
2023-10-01 11:33:18 -0600

    upgrade deps and improve website

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,80 +1,51 @@
 <script lang="ts">
 	import {base} from '$app/paths';
+	import {parse_package_meta} from '@fuz.dev/fuz_library/package.js';
+	import LibraryHeader from '@fuz.dev/fuz_library/LibraryHeader.svelte';
+	import LibraryFooter from '@fuz.dev/fuz_library/LibraryFooter.svelte';
 
-	// import {exports} from '$lib/exports';
-	// import {strip_end, strip_start} from '$lib/string';
-
-	// TODO glob import and print API?
-
-	// const exps = exports
-	// 	.map((e) => strip_end(`@grogarden/util/${strip_start(e, 'lib/')}`, '/index.ts'))
-	// 	.filter(Boolean);
+	// TODO SvelteKit warns about this but we put `/static` in `/src` because of what it's saying,
+	/// maybe change to import as the first item from `packages`
+	import package_json from '../static/.well-known/package.json';
+	const pkg = parse_package_meta(package_json.homepage, package_json);
 </script>
 
-<main class="box width_md">
-	<section class="padded_md">
-		<header>
-			<h1 class="row">@grogarden/util</h1>
-		</header>
-	</section>
-	<section class="box padded_md">
-		<blockquote style:padding="0" class="spaced">
-			<a class="padded_md" href="https://github.com/grogarden/util">source on GitHub</a>
-		</blockquote>
-		<code
-			><a class="padded_md" href="https://www.npmjs.com/package/@grogarden/util"
-				>npm i -D @grogarden/util</a
-			></code
-		>
-	</section>
-	<section class="prose padded_md width_sm">
-		<aside>
-			This docs website is under construction. For now please see <a
-				href="https://github.com/grogarden/util">the GitHub repo</a
-			>.
-		</aside>
-	</section>
-	<!-- <section class="padded_md"> -->
-	<!-- {#each exps as exp, i}<li class="prose">
-				<code class="padded_sm"
-					>import {'{'}...} from '<a
-						href="https://github.com/grogarden/util/blob/main/src/{exports[i]}">{exp.trim()}</a
-					>'</code
+<main class="box width_full">
+	<div class="box width_md">
+		<section>
+			<LibraryHeader {pkg} />
+		</section>
+		<section class="padded_md width_sm">
+			<blockquote class="box">
+				<div class="spaced">under construction</div>
+				<a href="https://www.grogarden.org/" title="grogarden.org" class="box padded_md panel"
+					><img
+						class="icon"
+						src="{base}/favicon.png"
+						alt="a pixelated green oak acorn with a glint of sun"
+					/></a
 				>
-			</li>{/each} -->
-	<!-- </section> -->
-	<footer class="box">
-		<a href="https://www.grogarden.org/" title="grogarden.org" class="box padded_md panel spaced"
-			><img
-				class="icon"
-				src="{base}/favicon.png"
-				alt="a pixelated green oak acorn with a glint of sun"
-			/>
-			<div>grogarden.org</div></a
-		>
-	</footer>
+			</blockquote>
+		</section>
+		<section>
+			<LibraryFooter {pkg} root_url="https://www.grogarden.org/" />
+		</section>
+	</div>
 </main>
 
 <style>
 	main {
+		margin-bottom: var(--spacing_xl5);
+	}
+	section {
+		margin-top: var(--spacing_xl3);
+		margin-bottom: var(--spacing_xl3);
 		display: flex;
 		flex-direction: column;
 		align-items: center;
-		margin: 0 auto;
-	}
-	h1 {
-		text-align: center;
-	}
-	footer {
-		font-size: var(--font_size_xl);
-		margin-top: var(--spacing_xl5);
-	}
-	footer > *:first-child {
-		font-size: var(--font_size_xl5);
 	}
 	.icon {
-		width: 64px;
-		height: 64px;
-		margin-bottom: var(--spacing_sm);
+		width: 80px;
+		height: 80px;
 	}
 </style>
