0aa3e8e6b5c969a2ec35eb9500f53a8e7f825d9d
Ryan Atkinson <mail@ryanatkn.com>
2023-10-31 14:31:29 -0600

    check

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,35 +1,35 @@
 <script lang="ts">
-	import Package_Detail from '@fuz.dev/fuz_library/Package_Detail.svelte';
-	import {parse_package_meta} from '@fuz.dev/fuz_library/package_meta.js';
-	import Library_Footer from '@fuz.dev/fuz_library/Library_Footer.svelte';
-	import Breadcrumb from '@fuz.dev/fuz_library/Breadcrumb.svelte';
+	// import Package_Detail from '@fuz.dev/fuz_library/Package_Detail.svelte';
+	// import {parse_package_meta} from '@fuz.dev/fuz_library/package_meta.js';
+	// import Library_Footer from '@fuz.dev/fuz_library/Library_Footer.svelte';
+	// import Breadcrumb from '@fuz.dev/fuz_library/Breadcrumb.svelte';
 
-	import {package_json} from '$lib/package.js';
-	import Favicon from '$routes/Favicon.svelte';
+	// import {package_json} from '$lib/package.js';
+	// import Favicon from '$routes/Favicon.svelte';
 
 	// TODO hacky - maybe put in context?
-	const pkg = parse_package_meta(package_json.homepage, package_json);
+	// const pkg = parse_package_meta(package_json.homepage, package_json);
 </script>
 
 <main class="width_md">
 	<div class="prose">
 		<section>
 			<header class="box">
-				<h1>{pkg.repo_name}</h1>
+				<!-- <h1>{pkg.repo_name}</h1> -->
 			</header>
-			<Breadcrumb><Favicon /></Breadcrumb>
+			<!-- <Breadcrumb><Favicon /></Breadcrumb> -->
 		</section>
 	</div>
 	<section class="box width_full spaced">
 		<div class="panel padded_md width_md">
-			<Package_Detail {pkg} />
+			<!-- <Package_Detail {pkg} /> -->
 		</div>
 	</section>
 	<section class="box">
-		<Library_Footer {pkg} />
+		<!-- <Library_Footer {pkg} /> -->
 	</section>
 	<section class="box spaced">
-		<Breadcrumb><Favicon /></Breadcrumb>
+		<!-- <Breadcrumb><Favicon /></Breadcrumb> -->
 	</section>
 </main>
 
@@ -40,9 +40,9 @@
 		align-items: center;
 		margin: 0 auto;
 	}
-	h1 {
+	/* h1 {
 		display: flex;
 		align-items: baseline;
 		text-align: center;
-	}
+	} */
 </style>
