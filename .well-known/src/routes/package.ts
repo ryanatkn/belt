7add38735d23629a81f88099d5e053f4a1c0070f
Ryan Atkinson <mail@ryanatkn.com>
2024-02-25 08:28:00 -0700

    deployment

--- .well-known/src/routes/package.ts
+++ .well-known/src/routes/package.ts
@@ -30,7 +30,7 @@ export const package_json = {
 		'@changesets/changelog-git': '^0.2.0',
 		'@ryanatkn/eslint-config': '^0.1.0',
 		'@ryanatkn/fuz': '^0.88.0',
-		'@ryanatkn/gro': '^0.112.2',
+		'@ryanatkn/gro': '^0.112.3',
 		'@sveltejs/adapter-static': '^3.0.1',
 		'@sveltejs/kit': '^2.5.1',
 		'@sveltejs/package': '^2.2.7',
