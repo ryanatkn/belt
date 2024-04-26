bc404c2fe5a8d0fa240a992ac1c76ec6952b93a6
Ryan Atkinson <mail@ryanatkn.com>
2024-04-25 20:40:58 -0600

    add a test for `count_iterator`

--- src/lib/iterator.test.ts
+++ src/lib/iterator.test.ts
@@ -6,4 +6,5 @@ import {count_iterator} from '$lib/iterator.js';
 test('count_iterator', () => {
 	assert.is(count_iterator('test'), 4);
 	assert.is(count_iterator([1, '2', false]), 3);
+	assert.is(count_iterator(new Intl.Segmenter().segment('abcde')), 5);
 });
