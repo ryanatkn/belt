a1f7b2502e5906cb7d3354c7cb5247a80c635b3f
Ryan Atkinson <mail@ryanatkn.com>
2024-06-24 07:59:35 -0600

    wip

--- src/lib/iterator.ts
+++ src/lib/iterator.ts
@@ -2,7 +2,7 @@
  * Useful for fast counting when `.length` is not available.
  */
 export const count_iterator = (iterator: Iterable<unknown>): number => {
-	var count = 0; // eslint-disable-line no-var
-	for (var _ of iterator) count++; // eslint-disable-line no-var
+	var count = 0;
+	for (var _ of iterator) count++;
 	return count;
 };
--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,2 +1,2 @@
 export const prerender = true;
-export const ssr = false; // TODO reenable, Svelte 5 hydration problem
+export const ssr = false; // TODO re-enable, Svelte 5 hydration problem
