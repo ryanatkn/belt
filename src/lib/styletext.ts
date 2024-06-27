bf4f8f96e622608b92de178f2ca719701a2cae95
Ryan Atkinson <mail@ryanatkn.com>
2024-06-27 17:05:47 -0600

    wip

--- src/lib/styletext.ts
+++ src/lib/styletext.ts
@@ -4,6 +4,8 @@ import {styleText} from 'node:util';
  * Re-exports Node's `styleText` util for convenience.
  */
 
+// TODO consider bright variants? any others?
+
 export const black = styleText.bind(null, 'black');
 export const red = styleText.bind(null, 'red');
 export const green = styleText.bind(null, 'green');
@@ -13,13 +15,15 @@ export const magenta = styleText.bind(null, 'magenta');
 export const cyan = styleText.bind(null, 'cyan');
 export const white = styleText.bind(null, 'white');
 export const gray = styleText.bind(null, 'gray');
-export const red_bright = styleText.bind(null, 'redBright');
-export const green_bright = styleText.bind(null, 'greenBright');
-export const yellow_bright = styleText.bind(null, 'yellowBright');
-export const blue_bright = styleText.bind(null, 'blueBright');
-export const magenta_bright = styleText.bind(null, 'magentaBright');
-export const cyan_bright = styleText.bind(null, 'cyanBright');
-export const white_bright = styleText.bind(null, 'whiteBright');
+export const black_bold = styleText.bind(null, ['black', 'bold']);
+export const red_bold = styleText.bind(null, ['red', 'bold']);
+export const green_bold = styleText.bind(null, ['green', 'bold']);
+export const yellow_bold = styleText.bind(null, ['yellow', 'bold']);
+export const blue_bold = styleText.bind(null, ['blue', 'bold']);
+export const magenta_bold = styleText.bind(null, ['magenta', 'bold']);
+export const cyan_bold = styleText.bind(null, ['cyan', 'bold']);
+export const white_bold = styleText.bind(null, ['white', 'bold']);
+export const gray_bold = styleText.bind(null, ['gray', 'bold']);
 export const bg_black = styleText.bind(null, 'bgBlack');
 export const bg_red = styleText.bind(null, 'bgRed');
 export const bg_green = styleText.bind(null, 'bgGreen');
@@ -29,10 +33,3 @@ export const bg_magenta = styleText.bind(null, 'bgMagenta');
 export const bg_cyan = styleText.bind(null, 'bgCyan');
 export const bg_white = styleText.bind(null, 'bgWhite');
 export const bg_gray = styleText.bind(null, 'bgGray');
-export const bg_red_bright = styleText.bind(null, 'bgRedBright');
-export const bg_green_bright = styleText.bind(null, 'bgGreenBright');
-export const bg_yellow_bright = styleText.bind(null, 'bgYellowBright');
-export const bg_blue_bright = styleText.bind(null, 'bgBlueBright');
-export const bg_magenta_bright = styleText.bind(null, 'bgMagentaBright');
-export const bg_cyan_bright = styleText.bind(null, 'bgCyanBright');
-export const bg_white_bright = styleText.bind(null, 'bgWhiteBright');
