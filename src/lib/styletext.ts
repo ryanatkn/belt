1261d3a4f94c0633ee76f74efd33bf7ade17c5aa
Ryan Atkinson <mail@ryanatkn.com>
2024-06-27 17:20:15 -0600

    wip

--- src/lib/styletext.ts
+++ src/lib/styletext.ts
@@ -2,9 +2,11 @@ import {styleText} from 'node:util';
 
 /**
  * Re-exports Node's `styleText` util for convenience.
+ * @see https://nodejs.org/docs/latest/api/util.html#utilstyletextformat-text
+ * @see https://nodejs.org/docs/latest/api/util.html#modifiers
  */
 
-// TODO consider bright variants? (they don't seem to do much) - any others?
+// TODO maybe include all modifiers and bright variants? for now just use `styleText` directly
 
 export const black = styleText.bind(null, 'black');
 export const red = styleText.bind(null, 'red');
@@ -15,15 +17,7 @@ export const magenta = styleText.bind(null, 'magenta');
 export const cyan = styleText.bind(null, 'cyan');
 export const white = styleText.bind(null, 'white');
 export const gray = styleText.bind(null, 'gray');
-export const black_bold = styleText.bind(null, ['black', 'bold']);
-export const red_bold = styleText.bind(null, ['red', 'bold']);
-export const green_bold = styleText.bind(null, ['green', 'bold']);
-export const yellow_bold = styleText.bind(null, ['yellow', 'bold']);
-export const blue_bold = styleText.bind(null, ['blue', 'bold']);
-export const magenta_bold = styleText.bind(null, ['magenta', 'bold']);
-export const cyan_bold = styleText.bind(null, ['cyan', 'bold']);
-export const white_bold = styleText.bind(null, ['white', 'bold']);
-export const gray_bold = styleText.bind(null, ['gray', 'bold']);
+
 export const bg_black = styleText.bind(null, 'bgBlack');
 export const bg_red = styleText.bind(null, 'bgRed');
 export const bg_green = styleText.bind(null, 'bgGreen');
@@ -33,3 +27,10 @@ export const bg_magenta = styleText.bind(null, 'bgMagenta');
 export const bg_cyan = styleText.bind(null, 'bgCyan');
 export const bg_white = styleText.bind(null, 'bgWhite');
 export const bg_gray = styleText.bind(null, 'bgGray');
+
+export const reset = styleText.bind(null, 'reset');
+export const bold = styleText.bind(null, 'bold');
+export const italic = styleText.bind(null, 'italic');
+export const overlined = styleText.bind(null, 'overlined');
+export const doubleunderline = styleText.bind(null, 'doubleunderline');
+export const framed = styleText.bind(null, 'framed');
