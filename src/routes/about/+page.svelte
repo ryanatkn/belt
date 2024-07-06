e9234af1665776f0b14393ae0cd6acba943ab8fc
Ryan Atkinson <mail@ryanatkn.com>
2024-07-05 23:49:33 -0600

    upgrade dev deps, enable ssr, and add community links on about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -2,6 +2,7 @@
 	import Package_Detail from '@ryanatkn/fuz/Package_Detail.svelte';
 	import {parse_package_meta} from '@ryanatkn/gro/package_meta.js';
 	import Library_Footer from '@ryanatkn/fuz/Library_Footer.svelte';
+	import Community_Links_Panel from '@ryanatkn/fuz/Community_Links_Panel.svelte';
 	import Breadcrumb from '@ryanatkn/fuz/Breadcrumb.svelte';
 
 	import {package_json, src_json} from '$routes/package.js';
@@ -17,6 +18,7 @@
 		</header>
 		<Breadcrumb>{package_json.glyph}</Breadcrumb>
 	</section>
+	<Community_Links_Panel />
 	<section class="box w_100 mb_lg">
 		<div class="panel p_md width_md">
 			<Package_Detail {pkg} />
