12fe677fef97feb68acdaec57e06d90764868064
Ryan Atkinson <mail@ryanatkn.com>
2024-07-15 09:55:41 -0600

    add a slugify test

--- src/lib/path.test.ts
+++ src/lib/path.test.ts
@@ -9,6 +9,7 @@ test('slugify', () => {
 	assert.is(slugify('AB'), 'ab');
 	assert.is(slugify('A B'), 'a-b');
 	assert.is(slugify('a, b!'), 'a-b');
+	assert.is(slugify("a's b"), 'as-b');
 	assert.is(slugify('a_b'), 'a_b');
 	assert.is(slugify('a_ b'), 'a_-b');
 	assert.is(slugify('a _ b'), 'a-_-b');
