515f0b625471a8372cc3d66ffcb75c9cfca72c83
Ryan Atkinson <mail@ryanatkn.com>
2024-07-15 15:01:58 -0600

    wip

--- src/routes/package.ts
+++ src/routes/package.ts
@@ -25,7 +25,7 @@ export const package_json = {
 		test: 'gro test',
 		preview: 'vite preview',
 		deploy: 'gro deploy',
-		benchmark: 'gro run src/benchmarks/slugify.ts',
+		benchmark: 'gro run src/benchmarks/slugify_benchmark.ts',
 	},
 	type: 'module',
 	engines: {node: '>=20.12'},
