56195a8fc786ea2eb31419de17cb6da7bbd60e91
Ryan Atkinson <mail@ryanatkn.com>
2024-07-19 07:28:00 -0600

    wip

---
+++ .changeset/rotten-flowers-build.md
@@ -0,0 +1,5 @@
+---
+'@ryanatkn/belt': patch
+---
+
+fix `attach_process_error_handlers` API to only handle exceptions
--- src/routes/package.ts
+++ src/routes/package.ts
@@ -33,8 +33,8 @@ export const package_json = {
 	devDependencies: {
 		'@changesets/changelog-git': '^0.2.0',
 		'@ryanatkn/eslint-config': '^0.4.2',
-		'@ryanatkn/fuz': '^0.110.4',
-		'@ryanatkn/gro': '^0.130.1',
+		'@ryanatkn/fuz': '^0.115.1',
+		'@ryanatkn/gro': '^0.133.1',
 		'@ryanatkn/moss': '^0.8.0',
 		'@sveltejs/adapter-static': '^3.0.2',
 		'@sveltejs/kit': '^2.5.18',
