5bb0b5c0365d46c19aa1cde85714dfb042627f6b
Ryan Atkinson <mail@ryanatkn.com>
2024-10-12 11:21:17 -0600

    upgrade dev deps

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import '@ryanatkn/moss/style.css';
 	import '@ryanatkn/moss/theme.css';
+	import '$routes/moss.css';
 	import '$routes/style.css';
 
 	import Themed from '@ryanatkn/fuz/Themed.svelte';
