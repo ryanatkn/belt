262a7245f446e2e7e17bacda4b04811cc3dcadc9
Ryan Atkinson <mail@ryanatkn.com>
2024-10-15 16:27:04 -0600

    improve `unreachable` type

--- src/lib/error.ts
+++ src/lib/error.ts
@@ -13,6 +13,6 @@ export class Unreachable_Error extends Error {
  * and therefore can't be used in places like Svelte markup without a workaround.
  * Made obsolete by this proposal: https://github.com/tc39/proposal-throw-expressions
  */
-export const unreachable = (value: never, message?: string): any => {
+export const unreachable = (value: never, message?: string): asserts value is never => {
 	throw new Unreachable_Error(value, message);
 };
