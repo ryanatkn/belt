57deeaafc068eb4280c839c594d3820b53b03917
Ryan Atkinson <mail@ryanatkn.com>
2025-08-02 07:02:59 -0400

    wip

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,4 @@ import {sveltekit} from '@sveltejs/kit/vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
-	// TODO delete after bootstrapping the build with updated deps
-	build: {
-		rollupOptions: {
-			external: [
-				'@ryanatkn/belt/pkg.js',
-				'@ryanatkn/belt/url.js',
-				'@ryanatkn/belt/string.js',
-				'@ryanatkn/belt/path.js',
-			],
-		},
-	},
 });
