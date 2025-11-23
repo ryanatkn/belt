9b9e2e9b73e4b4ba13a5824bfe62107b214d06a9
Ryan Atkinson <mail@ryanatkn.com>
2025-11-23 09:02:28 -0500

    wip

--- gro.config.ts
+++ gro.config.ts
@@ -3,7 +3,7 @@ import {gro_plugin_sveltekit_library} from '@ryanatkn/gro/gro_plugin_sveltekit_l
 import {gro_plugin_gen} from '@ryanatkn/gro/gro_plugin_gen.js';
 
 // Skip the SvelteKit app plugin to break circular dependency during builds
-const config: Create_Gro_Config = async (cfg) => {
+const config: Create_Gro_Config = (cfg) => {
 	cfg.plugins = () => [gro_plugin_gen(), gro_plugin_sveltekit_library()];
 	return cfg;
 };
